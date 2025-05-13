b8134845b67d67b1cdd9579560e386ddbbcea2d8
ciyek <aripurnomo606@gmail.com>
2025-05-13 22:19:37 +0700

    refactor: replace hardcoded API URL and key with environment variables

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -13,17 +13,13 @@ export interface NowPlayingResponse {
   results: Movie[];
 }
 
-const BASE_URL = "https://api.themoviedb.org/3";
-const API_KEY =
-  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMjIxZWY0NTgyMTRjNjU0MTIzNDQ5MjBmNGFjYWZlZiIsIm5iZiI6MTcyNDMyODMxMy4zNjIsInN1YiI6IjY2YzcyOTc5NjQ4MGFjZWZlM2VhYjg2MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zoeRd4v2lypTvsVlMz1iAB6ROSvk5SjpuFMwI8M3VgE";
-
 export const fetchNowPlayingMovies = async (): Promise<NowPlayingResponse> => {
   try {
     const response = await axios.get<NowPlayingResponse>(
-      `${BASE_URL}/movie/now_playing`,
+      `${process.env.NEXT_PUBLIC_BASE_URL}/movie/now_playing`,
       {
         headers: {
-          Authorization: `Bearer ${API_KEY}`,
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`,
         },
         params: {
           language: "en-US",
